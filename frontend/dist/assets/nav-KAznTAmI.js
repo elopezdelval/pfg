3b6151da02d3583dbbbde4ea840f261ba75df26e
Kikebu <elpzvl@gmail.com>
2026-04-05 21:17:38 +0200

    añadida lógica de mensajería y ajustes de layout

--- frontend/dist/assets/nav-KAznTAmI.js
+++
@@ -1,1 +0,0 @@
-function d(){const n=document.getElementById("desplegable"),e=document.getElementById("menu");e.hidden=!0,n.addEventListener("click",t=>{t.preventDefault(),e.hidden=!e.hidden})}export{d as i};
